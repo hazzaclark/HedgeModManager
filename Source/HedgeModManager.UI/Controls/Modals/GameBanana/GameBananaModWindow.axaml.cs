0f1203b0b01c16fab26775705c795201f301e057
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-05-30 16:03:42 +0100

    Add files via upload

---
+++ Source/HedgeModManager.UI/Controls/Modals/GameBanana/GameBananaModWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using HedgeModManager.UI.ViewModels;
+
+namespace HedgeModManager.UI.Controls.Modals.GameBanana
+{
+    public partial class GameBananaModWindow : UserControl
+    {
+        public GameBananaModWindow()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
